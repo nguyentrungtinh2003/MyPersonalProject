73df993aeae93ed2cb1f1763848f7743113e746a
nguyentrungtinh <trungtinhn300@gmail.com>
2024-07-15 20:26:14 +0700

    Upload project

---
+++ Project_Kanban_Backend/src/main/java/com/TrungTinhFullStack/Project_Kanban_Backend/ProjectKanbanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.Project_Kanban_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKanbanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKanbanBackendApplication.class, args);
+	}
+
+}
---
+++ Project_Kanban_Backend/src/test/java/com/TrungTinhFullStack/Project_Kanban_Backend/ProjectKanbanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.Project_Kanban_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKanbanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
